3dca78c5dfdc7ca8a5e3e3d0c4eb8320d6d41ba0
Adam-Szitas <example.eu.sk.number@gmail.com>
2024-11-27 18:40:57 +0100

    Editing new page to dist

---
+++ dist/budgeter/server/chunk-I7QZKUJW.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as p,b as r,c as i,h as n,k as m,m as a,n as f,q as s,w as c}from"./chunk-D67B5OUV.mjs";var o=class e{title="Budgeter";static \u0275fac=function(t){return new(t||e)};static \u0275cmp=p({type:e,selectors:[["app-root"]],standalone:!0,features:[i],decls:1,vars:0,template:function(t,A){t&1&&r(0,`This is the Budgeter!
+`)}})};var l=[];var u={providers:[n({eventCoalescing:!0}),c(l),f()]};var C={providers:[s()]},d=m(u,C);var v=()=>a(o,d),j=v;export{j as a};
--- dist/budgeter/server/main.server.mjs
+++ dist/budgeter/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ONON37MD.mjs";import"./chunk-FOWZ7GDG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-I7QZKUJW.mjs";import"./chunk-D67B5OUV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
