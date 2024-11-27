4ae3b0c2ada478ce0e3265b3ed37cbc1e2043737
Adam-Szitas <example.eu.sk.number@gmail.com>
2024-11-27 19:03:39 +0100

    Changing built project and added to global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Budgeter';
+
+  constructor(private httpClient: HttpClient){}
+
+  public testCall() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/todos/1').subscribe({
+      next: console.log
+    })
+  }
 }
