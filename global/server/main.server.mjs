ccbf7c232baeb4ef339493625337e7bbeb588d2f
Adam-Szitas <example.eu.sk.number@gmail.com>
2025-03-06 20:11:24 +0100

    BGTR-00x - base href

--- global/server/main.server.mjs
+++ global/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NS5C4535.mjs";import"./chunk-KTYSPMZU.mjs";import"./chunk-7NDJFHJM.mjs";import"./chunk-KMFYZL7R.mjs";import"./chunk-PXDRA6TY.mjs";import"./chunk-KGD56N57.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-EOQAANXE.mjs";import"./chunk-KTYSPMZU.mjs";import"./chunk-7NDJFHJM.mjs";import"./chunk-KMFYZL7R.mjs";import"./chunk-PXDRA6TY.mjs";import"./chunk-KGD56N57.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
